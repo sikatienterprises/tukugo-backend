a30544ab9ecf369508b0a90963f88b7a411c8d81
pradeepsikati <pradeep@sikati.in>
2025-05-28 19:12:34 +0530

    api otp

--- custom_auth/utils.py
+++ custom_auth/utils.py
@@ -1,13 +1,32 @@
-import random
-from django.utils import timezone
-from datetime import timedelta
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from django.conf import settings
+from decouple import config
+import urllib3
+urllib3.disable_warnings()
 
-def generate_otp():
-    return str(random.randint(100000, 999999))
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+# ------------------------- not for development use--------
+import ssl
+import certifi
+import os
 
-def send_otp_to_phone(phone, otp_code):
-    # Replace this with actual SMS logic using Twilio, MSG91, etc.
-    print(f"Sending OTP {otp_code} to {phone}")
+os.environ['SSL_CERT_FILE'] = certifi.where()
+#----------------------------------- worning---------------------------
+def send_otp_email(to_email, otp_code):
+    message = Mail(
+        from_email=config('DEFAULT_FROM_EMAIL'),
+        to_emails=to_email,
+        subject='Your OTP Code for TukuGO',
+        html_content=f'<p>Your OTP is <strong>{otp_code}</strong>. It is valid for 10 minutes.</p>'
+    )
 
-def get_otp_expiry():
-    return timezone.now() + timedelta(minutes=5)
+    try:
+        sg = SendGridAPIClient(config('SENDGRID_API_KEY_New'))
+        response = sg.send(message)
+        print(f"[SendGrid] Sent OTP to {to_email}: Status {response.status_code}")
+        return True
+    except Exception as e:
+        print(f"[SendGrid Error] Failed to send OTP: {str(e)}")
+        return False
